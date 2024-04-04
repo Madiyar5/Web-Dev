efd6e2a2f4ea5bccb3672408135ef03fcde4f7eb
Madiyar5 <113119977+Madiyar5@users.noreply.github.com>
2024-04-04 15:53:38 +0600

    lab8

---
+++ views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from shopback.models import Product, Category
+from django.http import JsonResponse
+
+# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    data = {
+        'products': list(products.values())
+    }
+    return JsonResponse(data)
+
+def product(request, id):
+    products = Product.objects.get(id = id)
+    data = {
+        'product': {
+            'name': products.name,
+            'price': products.price,
+            'description': products.description,
+            'count': products.count,
+            'is_active': products.is_active,
+        }
+    }
+    return JsonResponse(data)
+
+def category(request, id):
+    categoriess = Category.objects.get(id = id)
+    data = {
+        'category': {
+            'name': categoriess.name,
+        }
+    }
+    return JsonResponse(data)
+
+def categories(request):
+    categoriess = Category.objects.all()
+    data = {
+        'categories': list(categoriess.values())
+    }
+    return JsonResponse(data)
+
+def prod_in_cat(request, id):
+    prods = Product.objects.filter(id = id)
+    data = {
+        'products' : list(prods.values())
+    }
+    return JsonResponse(data)
